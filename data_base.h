8424ec0a0899306315eb473fd0c46d76e91d5adf
Johann Hahn <johann-hahn@yandex.ru>
2024-01-23 14:56:29 +0100

    database integration

--- data_base.h
+++ data_base.h
@@ -11,26 +11,30 @@
 
 struct Result
 {
-	std::string entries;
-	std::string col_names;
+    Result();
+    Result(std::string entries, std::string col_names, size_t num_cols, size_t num_rows);
+    std::string entries;
+    std::string col_names;
+    size_t num_cols;
+    size_t num_rows;
+    void print(const char* prefix = "");
 };
 
 class DataBase
 {
 public:
-	
-	DataBase(const char* file);
-	~DataBase();
-	const Result& sql_query(const char* query, ...);
-	const Result& select(const char* tables, const char* cols, const char* tail = "");
-	void save_db();
-	void rollback_db();
-	bool save_needed = false;
+    DataBase(const char* file);
+    ~DataBase();
+    const Result& sql_query(const char* query, ...);
+    const Result& select(const char* tables, const char* cols, const char* tail = "");
+    void save_db();
+    void rollback_db();
+    bool save_needed = false;
 private:
-	sqlite3* db;
-	std::unordered_map<std::string, Result> cache;
-	// the callback gets called for every entry yogtu get from the database
-	static int callback_result(void* data, int argc, char** argv, char** azColName);
-	uint32_t sql_exec(const char* sql, int(*callback)(void*, int, char**, char**), void* data);
-	bool trans_start = true;
+    sqlite3* db;
+    std::unordered_map<std::string, Result> cache;
+    // the callback gets called for every entry yogtu get from the database
+    static int callback_result(void* data, int argc, char** argv, char** azColName);
+    uint32_t sql_exec(const char* sql, int(*callback)(void*, int, char**, char**), void* data);
+    bool trans_start = true;
 };
--- main.cpp
+++ main.cpp
@@ -1,23 +1,39 @@
 #include "raylib-gui-layout/gui.h"
 #include "raylib-gui-layout/layout.h"
 #include "data_base.h"
+#include "raylib-gui-layout/raylib/src/raylib.h"
 
 float window_width = 900;
 float window_height = 600;
+Rectangle main_window_rec = Rectangle{0, 0, window_width, window_height};
+Layout main_window_layout = Layout(main_window_rec, SLICE_HOR, 0.5f);
 
 void on_click(void* label) {
     std::cout << (char*)label << "\n";
 }
 
+void resize() {
+    window_width = 900;
+    window_height = 600;
+    main_window_rec = Rectangle{0, 0, window_width, window_height};
+    main_window_layout = Layout(main_window_rec, SLICE_HOR, 0.5f);
+}
+
 int main() {
     InitWindow(window_width, window_height, "Home DataBase");
+    SetWindowState(FLAG_WINDOW_RESIZABLE);
     DataBase db = DataBase("datenbank.db");
     Result r = db.sql_query("Select name, category_id from things");
-    std::cout << r.entries << ", " << r.col_names << "\n"; 
+    r.print("after query: \n");
+    resize();
+    SetTargetFPS(60);
     while(!WindowShouldClose()) {
+	if(IsWindowResized()) {
+	    resize();
+	}
 	BeginDrawing();
 	ClearBackground(RED);
-	Gui::table(Rectangle{100, 100, 200, 200}, 3, 2, r.col_names.c_str(), r.entries.c_str(), 0, on_click);
+	Gui::table(main_window_layout.get_slot(0), r.num_cols, r.num_rows, r.col_names.c_str(), r.entries.c_str(), on_click);
 	EndDrawing();
     }
 }
